d952fa4465aae9dc3369969f51f67a8f9aba10e3
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-03-25 12:52:38 +0600

    completed

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const tabInactive=["bg-transparent","text-black","border-state-200"];
 const allContainer=document.getElementById("all-container");
 const interviewContainer=document.getElementById("interview-container");
 const rejectContainer=document.getElementById("reject-container");
+const emptyStatus=document.getElementById("empty-status");
 
 function switchTab(tab){
     const tabs=["all","interview","rejected"];
+    currentTab=tab;
     for (const t of tabs) {
     const tabName=document.getElementById("tab-"+t);  
     if(t===tab)
@@ -23,19 +25,36 @@ const pages=[allContainer,interviewContainer,rejectContainer];
 for (const page of pages) {
     page.classList.add("hidden");
 }
+emptyStatus.classList.add("hidden");
 if(tab=="all")
 {
     allContainer.classList.remove("hidden");
+    if(allContainer.children.length<1)
+    {
+        emptyStatus.classList.remove("hidden");
+    }
 }
 else if(tab=="interview")
 {
     interviewContainer.classList.remove("hidden");
+     if(interviewContainer.children.length<1)
+    {
+        emptyStatus.classList.remove("hidden");
+    }
 }else
+{
     rejectContainer.classList.remove("hidden");
+     if(rejectContainer.children.length<1)
+    {
+        emptyStatus.classList.remove("hidden");
+    }
+}
+    updateStat();
 }
 const totalStat=document.getElementById("stat-total");
 const interviewStat=document.getElementById("stat-interview");
 const rejectStat=document.getElementById("stat-reject");
+const available=document.getElementById("available");
 totalStat.innerText=allContainer.children.length;
 switchTab(currentTab);
 
@@ -47,13 +66,30 @@ document.getElementById("jobs-container").addEventListener("click",function(even
     if(clickedElement.classList.contains("interview"))
     {   status.innerText="interviewed";
         interviewContainer.appendChild(card);
+        updateStat();
     }
     if(clickedElement.classList.contains("reject"))
     {   status.innerText="rejected";
         rejectContainer.appendChild(card);
+        updateStat();
     }
     if(clickedElement.classList.contains("delete"))
     {
         parent.removeChild(card);
+        updateStat();
     }
 })
+function updateStat(){
+   const counts={
+    all:allContainer.children.length,
+    interview:interviewContainer.children.length,
+    rejected:rejectContainer.children.length,
+   };
+    totalStat.innerText=counts.all;
+    interviewStat.innerText=counts.interview;
+    rejectStat.innerText=counts.rejected;
+   available.innerText=counts[currentTab];
+   
+
+}
+updateStat();
